f2d79efb1b8e80d7befbae2a530bbd2e8cb9e888
KazeMae <xiaochunfeng.x@foxmail.com>
2025-04-13 22:01:15 +0800

    Initial commit

---
+++ src/test/java/fun/kazex/dwz/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.kazex.dwz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
